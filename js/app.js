e6272744f6c977c973cc877c4c2844d98f1168e6
john <>
2021-01-12 18:57:54 +0100

    Add service worker

---
+++ js/app.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then((reg) => console.log('sw register'))
+        .catch((err) => console.lof('sw not register'));
+}
---
+++ sw.js
@@ -0,0 +1,47 @@
+const staticCacheName = 'site-static-v2';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/t2.png',
+];
+
+// install sw
+self.addEventListener('install', evt => {
+    // console.log('sw has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    // console.log('sw has been activated');
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
+});
+
+//fetch event
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cachesRes => {
+            return cachesRes || fetch(evt.request);
+        })
+    );
+});
